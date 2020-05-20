41df5c6d765d7e80317e1cf52eab2be8881963d2
Saikat Dutta <saikatdutta@Saikats-MacBook-Air.local>
2020-05-20 19:21:06 +0530

    pg added

--- src/main/app/models/user.js
+++ src/main/app/models/user.js
@@ -1,30 +1,19 @@
-// const _ = require("lodash");
-// const Base = require("./base");
+const _ = require("lodash");
+const Base = require("./base");
 
-// class User extends Base {
-//   static get tableName() {
-//     return "users";
-//   }
+class User extends Base {
+  static get tableName() {
+    return "users";
+  }
 
-//   get $secureFields() {
-//     return [];
-//   }
+  get $secureFields() {
+    return [];
+  }
 
-//   $formatJson(json, options) {
-//     json = super.$formatJson(json, options);
-//     return _.omit(json, this.$secureFields);
-//   }
+  $formatJson(json, options) {
+    json = super.$formatJson(json, options);
+    return _.omit(json, this.$secureFields);
+  }
+}
 
-//   static relationMappings = {
-//     social_account: {
-//       relation: Base.HasOneRelation,
-//       modelClass: `${__dirname}/socialAccount`,
-//       join: {
-//         from: "users.id",
-//         to: "user_social_accounts.user_id"
-//       }
-//     }
-//   };
-// }
-
-// module.exports = User;
+module.exports = User;
